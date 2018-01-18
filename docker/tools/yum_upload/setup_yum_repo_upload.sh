bd067b02d3ace7791dd9cf20e8faa1c559c0bf92
Kenneth Lareau <kenneth.lareau@couchbase.com>
2018-01-17 16:38:59 -0800

    CBD-2279: More entrypoint script fixes
    Change-Id: Ia6c165d9324361a505e931b98f6ad805b217c2bf
    Reviewed-on: http://review.couchbase.org/87998
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

--- docker/tools/yum_upload/setup_yum_repo_upload.sh
+++ docker/tools/yum_upload/setup_yum_repo_upload.sh
@@ -1,20 +1,25 @@
-#!/bin/bash
+#!/bin/bash -e
 
 mkdir -p /home/couchbase/.aws /home/couchbase/.ssh
 
 # Copy needed GPG and AWS credential files from /ssh into .ssh and .aws,
 # ensure ownership and permissions are correct
-if [[ -d /ssh ]] && [[ "$(ls -A /ssh)" ]]
+if [[ ! -d /ssh ]] || [[ ! "$(ls -A /ssh)" ]]
 then
+    echo "Missing key configuration files in /ssh"
+    exit 1
+else
     cp -a /ssh/*.gpg /home/couchbase/.ssh
     cp -a /ssh/GPG-KEY-COUCHBASE-1.0 /home/couchbase/.ssh
     cp -a /ssh/aws-credentials /home/couchbase/.aws/credentials
 fi
+
 chown -R couchbase:couchbase /home/couchbase/.aws /home/couchbase/.ssh
 chmod 600 /home/couchbase/.aws/* /home/couchbase/.ssh/*
 
 # Run repo_upload program with 'yum' option
 [[ "$1" == "default" ]] && {
+    cd /home/couchbase
     exec /usr/local/bin/repo_upload -c /etc/repo_upload.ini -r yum -e $EDITION
 }
 
