ddc6bf5d34f885ea581154f33ad4bb24b71588bb
Chris Hillery <ceej@couchbase.com>
2018-01-29 01:17:50 -0800

    Updates to SDK Docker swarm buildslaves
     - Update to Jenkins Swarm jar 3.8
     - Specify slave name based on hostname, to enable other processes in
       the container to determine slave name
     - Set slaves to "exclusive" mode
     - Improve slave->master connection (5 retries, clean duplicate slaves)
     - Completed Ubuntu 16.04 slave implementation
    
    Change-Id: Ia3c05afb58c95469724e5009103ca7229aa4d566
    Reviewed-on: http://review.couchbase.org/88482
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/buildslaves/util/couchbuilder_start.sh
+++ docker/buildslaves/util/couchbuilder_start.sh
@@ -8,7 +8,7 @@
 #   JENKINS_MASTER
 #   JENKINS_SLAVE_ROOT (defaults to /home/couchbase/jenkins)
 #   JENKINS_SLAVE_EXECUTORS (defaults to 1)
-#   JENKINS_SLAVE_NAME
+#   JENKINS_SLAVE_NAME (base name; will have container ID appended)
 #   JENKINS_SLAVE_LABELS
 #
 # In addition it expects the following Docker secrets to exist:
@@ -26,14 +26,17 @@ done
 # if first argument is "swarm", run the (Jenkins) swarm jar with any arguments
 [[ "$1" == "swarm" ]] && {
     shift
-    set -x
 
     exec java $JAVA_OPTS -jar /usr/local/lib/swarm-client.jar \
        -fsroot "${JENKINS_SLAVE_ROOT:-/home/couchbase/jenkins}" \
        -master "${JENKINS_MASTER}" \
+       -mode exclusive \
        -executors "${JENKINS_SLAVE_EXECUTORS:-1}" \
-       -name "${JENKINS_SLAVE_NAME}" \
+       -name "${JENKINS_SLAVE_NAME}-$(hostname)" \
+       -disableClientsUniqueId \
+       -deleteExistingClients \
        -labels "${JENKINS_SLAVE_LABELS}" \
+       -retry 5 \
        -username "$(cat /run/secrets/jenkins_master_username)" \
        -password "$(cat /run/secrets/jenkins_master_password)"
 }
