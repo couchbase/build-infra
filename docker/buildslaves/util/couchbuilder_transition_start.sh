f3d876fe5a42eb495706cff7e567b2d6aaf562f1
Chris Hillery <ceej@couchbase.com>
2018-05-07 18:30:46 -0700

    CBD-2468: Create Docker Swarm-based set of Server build slave
    Migrated slave Dockerfiles from build repository. Extended to allow running
    either as traditional ssh-based slaves or Jenkins Swarm-based slaves. Added
    Docker Swarm stackfile to launch set of slave instances.
    
    Change-Id: If4fad9a47ed4b8dda68a4207a156fcd246915830
    Reviewed-on: http://review.couchbase.org/93843
    Reviewed-by: Kenneth Lareau <kenneth.lareau@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/buildslaves/util/couchbuilder_transition_start.sh
+++ docker/buildslaves/util/couchbuilder_transition_start.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
 # Script intended to be ENTRYPOINT for Couchbase build containers
+# based on Jenkins Swarm and running on Docker Swarm, OR as a
+# "traditional" ssh-based based slave.
+
+# When invoked as a Jenkins Swarm slave, it expects
+# the following environment variables to be set (by the Dockerfile
+# or the service):
+#
+#   JENKINS_MASTER
+#   JENKINS_SLAVE_ROOT (defaults to /home/couchbase/jenkins)
+#   JENKINS_SLAVE_EXECUTORS (defaults to 1)
+#   JENKINS_SLAVE_NAME (base name; will have container ID appended)
+#   JENKINS_SLAVE_LABELS
+#
+# In addition it expects the following Docker secrets to exist:
+#
+#   /run/secrets/jenkins_master_username
+#   /run/secrets/jenkins_master_password
 
 # First, copy any files in /ssh to /home/couchbase/.ssh, changing ownership to
-# user couchbase and maintaining permissions
+# user couchbase and maintaining permissions. This is for "old-school" slaves
+# which aren't using Docker Swarm secrets exclusively yet.
 if [ -d /ssh ] && [ "$(ls -A /ssh)" ]
 then
     cp -a /ssh/* /home/couchbase/.ssh
@@ -11,16 +29,46 @@ fi
 chown -R couchbase:couchbase /home/couchbase/.ssh
 chmod 600 /home/couchbase/.ssh/*
 
-# Hook for build image-specific steps
+# Hooks for build image-specific steps
+shopt -s nullglob
+for hook in /usr/sbin/couchhook.d/*
+do
+    "${hook}"
+done
+
+# Older single hook for build image-specific steps
 if [[ -e /usr/sbin/couchhook.sh ]]
 then
     /usr/sbin/couchhook.sh
 fi
 
-# Start sshd (as new, long-running, foreground process)
+# if first argument is "swarm", run the (Jenkins) swarm jar with any arguments
+[[ "$1" == "swarm" ]] && {
+    shift
+
+    exec sudo -u couchbase -H \
+       PATH=/usr/local/bin:/usr/bin:/bin \
+       java $JAVA_OPTS \
+       -jar /usr/local/lib/swarm-client.jar \
+       -fsroot "${JENKINS_SLAVE_ROOT:-/home/couchbase/jenkins}" \
+       -master "${JENKINS_MASTER}" \
+       -mode exclusive \
+       -executors "${JENKINS_SLAVE_EXECUTORS:-1}" \
+       -name "${JENKINS_SLAVE_NAME}-$(hostname)" \
+       -disableClientsUniqueId \
+       -deleteExistingClients \
+       -labels "${JENKINS_SLAVE_LABELS}" \
+       -retry 5 \
+       -username "$(cat /run/secrets/jenkins_master_username)" \
+       -password "$(cat /run/secrets/jenkins_master_password)"
+}
+
+# if first argument is "default", for backwards-compatibility start sshd
+# (as new, long-running, foreground process)
 [[ "$1" == "default" ]] && {
     exec /usr/sbin/sshd -D
 }
 
+# If argument is not 'swarm', assume user want to run their own process,
+# for example a `bash` shell to explore this image
 exec "$@"
-
