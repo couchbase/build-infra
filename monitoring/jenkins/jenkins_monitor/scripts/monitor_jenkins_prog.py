42e7d984b4c5ef5f628b9c79f72b54425905edb8
Kenneth Lareau <kenneth.lareau@couchbase.com>
2018-07-03 21:44:46 -0700

    CBD-2580: Add rate limiting for email notifications
    Added a SQLite database backend to keep track of the offline nodes and
    stuck jobs, along with a 'checked' count so that email notifications
    can be rate limited to prevent 'spam floods'.
    
    Also added a minimum time of 5 minutes for offline nodes and stuck jobs
    to remove some false positives.
    
    Change-Id: Ie2932ba6927d0eb65a6f1166f2beb3e4b5f66a49
    Reviewed-on: http://review.couchbase.org/96494
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

--- monitoring/jenkins/jenkins_monitor/scripts/monitor_jenkins_prog.py
+++ monitoring/jenkins/jenkins_monitor/scripts/monitor_jenkins_prog.py
@@ -9,9 +9,12 @@ More capability will be added as needed.
 """
 
 import argparse
+import datetime
 import json
 import logging
+import pathlib
 import smtplib
+import sqlite3
 import statistics
 import sys
 import time
@@ -28,6 +31,10 @@ logger.setLevel(logging.INFO)
 
 ch = logging.StreamHandler()
 logger.addHandler(ch)
+
+
+def adapt_datetime(ts):
+    return time.mktime(ts.timetuple())
 
 
 def send_email(smtp_server, receivers, message):
@@ -52,12 +59,12 @@ def send_email(smtp_server, receivers, message):
 
 
 class JenkinsMonitor:
-    """"""
+    """Handle basic Jenkins monitoring"""
 
     def __init__(self, email_info, server_info):
         """
         Basic initialization and loading of key information for all builds
-        from Jenkins
+        from Jenkins, along with preparing state database
         """
 
         self.smtp_server = email_info['smtp_server']
@@ -70,10 +77,38 @@ class JenkinsMonitor:
 
         self.session = requests_xml.XMLSession()
 
+        sqlite3.register_adapter(datetime.datetime, adapt_datetime)
+        db_file = pathlib.Path.home() / 'monitoring.db'
+        self.conn = sqlite3.connect(str(db_file))
+        self.cursor = self.conn.cursor()
+        self.initialize_db()
+
         try:
             self.builds, self.running = self.get_full_build_info()
         except (ConnectionError, ValueError) as exc:
             raise RuntimeError(exc)
+
+    def initialize_db(self):
+        """
+        Create the 'slaves' and 'jobs' tables if they don't already
+        exist in the database
+        """
+
+        self.cursor.execute(
+            "select name from sqlite_master where type = 'table';"
+        )
+        tables = [row[0] for row in self.cursor.fetchall()]
+
+        if 'slaves' not in tables:
+            self.conn.execute(
+                "create table slaves "
+                "(name text, offline_since timestamp, checked int)"
+            )
+
+        if 'jobs' not in tables:
+            self.conn.execute(
+                "create table jobs (name text, num int, checked int)"
+            )
 
     def get_jenkins_data(self, url_path):
         """
@@ -106,8 +141,8 @@ class JenkinsMonitor:
         """
 
         job_list = self.get_jenkins_data(
-            f'/api/xml?tree=jobs[displayName,builds[building,timestamp,'
-            f'estimatedDuration,duration,url,result]]'
+            f'/api/xml?tree=jobs[displayName,builds[number,building,'
+            f'timestamp,estimatedDuration,duration,url,result]]'
         )['hudson']['job']
 
         builds = dict()
@@ -134,6 +169,141 @@ class JenkinsMonitor:
 
         return builds, running
 
+    def check_offline_time(self, node):
+        """
+        Return time and number of checks for an offline node;
+        return None if node was not previously offline
+        """
+
+        self.cursor.execute(
+            "select offline_since, checked from slaves where name=?", (node,)
+        )
+        res = self.cursor.fetchone()
+
+        if res is None:
+            return None
+
+        now = datetime.datetime.now()
+
+        return time.mktime(now.timetuple()) - res[0], res[1]
+
+    def add_offline_node(self, node):
+        """
+        Add node that has just gone offline, setting number of times
+        checked to 0 (not checked yet)
+        """
+
+        now = datetime.datetime.now()
+
+        try:
+            with self.cursor:
+                self.cursor.execute(
+                    "insert into slaves values(?, ?, ?)", (node, now, 0)
+                )
+        except sqlite3.IntegrityError:
+            raise RuntimeError(f'Node {node} already in slaves table')
+
+    def update_offline_node(self, node, checked):
+        """
+        Update an offline node with a new number of times checked
+        """
+
+        try:
+            with self.cursor:
+                self.cursor.execute(
+                    "update slaves set checked=? where name=?",
+                    (checked, node)
+                )
+        except sqlite3.IntegrityError as exc:
+            raise RuntimeError(exc)
+
+    def clear_offline_nodes(self, offline):
+        """Remove any nodes that are no longer offline"""
+
+        self.cursor.execute("select name from slaves")
+
+        for row in self.cursor.fetchall():
+            node = row[0]
+
+            if node not in offline:
+                try:
+                    with self.cursor:
+                        self.cursor.execute(
+                            "delete from slaves where name=?", (node,)
+                        )
+                except sqlite3.IntegrityError as exc:
+                    raise RuntimeError(exc)
+
+    def find_job(self, name, build_num):
+        """
+        Find given job and build number in database, return None
+        if not found
+        """
+
+        self.cursor.execute(
+            "select checked from jobs where name=? and num=?",
+            (name, build_num)
+        )
+        res = self.cursor.fetchone()
+
+        return None if res is None else res[0]
+
+    def add_stuck_job(self, name, build_num):
+        """
+        Add job that has just gotten stuck, setting number of times
+        checked to 0 (not checked yet)
+        """
+
+        try:
+            with self.cursor:
+                self.cursor.execute(
+                    "insert into jobs values(?, ?, ?)", (name, build_num, 0)
+                )
+        except sqlite3.IntegrityError as exc:
+            raise RuntimeError(exc)
+
+    def update_stuck_job(self, name, build_num, checked):
+        """
+        Update a stuck job with a new number of times checked
+        """
+
+        try:
+            with self.cursor:
+                self.cursor.execute(
+                    "update jobs set checked=? where name=? and num=?",
+                    (checked, name, build_num)
+                )
+        except sqlite3.IntegrityError as exc:
+            raise RuntimeError(exc)
+
+    def clear_stuck_jobs(self):
+        """Remove any jobs no longer in the stuck state"""
+
+        self.cursor.execute("select name, num from jobs")
+
+        for row in self.cursor.fetchall():
+            name, build_num = row
+            builds = self.running.get(name)
+            delete = False
+
+            if builds is None:
+                delete = True
+
+            job_nums = [build['number'] for build in builds]
+
+            if build_num in job_nums:
+                delete = True
+
+            if delete:
+                try:
+                    with self.cursor:
+                        self.cursor.execute(
+                            "delete from jobs where name=? and num=?",
+                            (name, build_num)
+                        )
+                except sqlite3.IntegrityError as exc:
+                    raise RuntimeError(exc)
+
     def get_max_time(self, job_name):
         """
         Return a time (in seconds) of the maximum "acceptable" time
@@ -142,7 +312,8 @@ class JenkinsMonitor:
 
         The value is calculated by adding an additional standard
         deviation over the largest one from the historical values
-        to find the maximum time to allow
+        to find the maximum time to allow; if this is not at least
+        300 seconds, return that instead
         """
 
         times = list()
@@ -155,7 +326,7 @@ class JenkinsMonitor:
         std_dev = int(statistics.stdev(times))
         max_deviation = max(abs(t - t_mean) // std_dev + 1 for t in times)
 
-        return (t_mean + (max_deviation + 1) * std_dev) / 1000
+        return max((t_mean + (max_deviation + 1) * std_dev) / 1000, 300)
 
     def check_nodes(self):
         """Ensure all nodes are online, alert via email if any are down"""
@@ -180,8 +351,21 @@ class JenkinsMonitor:
 
         offline = [system['displayName']['$'] for system in systems]
 
-        if offline:
-            for node in offline:
+        # For each offline node, check to see if it's been offline
+        # and for how long; if it wasn't already marked as offline,
+        # add it to the database and continue, else look for a checked
+        # count of a multiple of 20 (one hour based on current run
+        # spacing) and an offline time of more than 5 minutes and
+        # email as necessary, then update node info in database (if
+        # offline for more than 5 minutes)
+        for node in offline:
+            off_time, checked = self.check_offline_time(node)
+
+            if off_time is None:
+                self.add_offline_node(node)
+                continue
+
+            if checked % 20 == 0 and off_time > 300:
                 message = {
                     'subject': f'Node {node} on Jenkins server '
                                f'{self.server_name} is OFFLINE',
@@ -190,6 +374,12 @@ class JenkinsMonitor:
                 send_email(
                     self.smtp_server, self.receivers, message
                 )
+
+            if off_time > 300:
+                self.update_offline_node(node, checked + 1)
+
+        # Remove from database any nodes no longer offline
+        self.clear_offline_nodes(offline)
 
     def check_running_builds(self):
         """
@@ -206,16 +396,36 @@ class JenkinsMonitor:
                     time.time() - build['timestamp'] / 1000
                 )
 
+                # If the build time is more than the maximum allowed time,
+                # look for the job in the database, adding it if it's not
+                # there, then look for a checked count of a multiple of 20
+                # (one hour based on current run spacing) and email as
+                # necessary, then update stuck job in the database
                 if curr_build_time > max_time:
-                    message = {
-                        'subject': f'Job {job_name} on Jenkins server '
-                                   f'{self.server_name} is STALLED',
-                        'body': f'Job has taken {curr_build_time} seconds '
-                                f'to run so far.\nPlease investigate issue'
-                    }
-                    send_email(
-                        self.smtp_server, self.receivers, message
-                    )
+                    build_num = int(build['number'])
+                    checked = self.find_job(job_name, build_num)
+
+                    if checked is None:
+                        self.add_stuck_job(job_name, build_num)
+                        checked = 0
+
+                    if checked % 20 == 0:
+                        message = {
+                            'subject': f'Build {build_num} for job '
+                                       f'{job_name} on Jenkins server '
+                                       f'{self.server_name} is STALLED',
+                            'body': f'Job has taken {curr_build_time} '
+                                    f'seconds to run so far.\nPlease '
+                                    f'investigate issue'
+                        }
+                        send_email(
+                            self.smtp_server, self.receivers, message
+                        )
+
+                    self.update_stuck_job(job_name, build_num, checked + 1)
+
+        # Remove from database any jobs no longer stuck
+        self.clear_stuck_jobs()
 
 
 def main():
