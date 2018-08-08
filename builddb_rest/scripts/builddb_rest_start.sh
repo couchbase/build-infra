46c63d3e6639395f85a73c59f6e978ff7876388d
Kenneth Lareau <kenneth.lareau@couchbase.com>
2018-08-08 16:11:32 -0700

    CBD-2563: Support product metadata requirement in builddb_rest
    Change-Id: I59bc782843b594930f38a056724ad1ef920f35ba
    Reviewed-on: http://review.couchbase.org/97894
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

---
+++ builddb_rest/scripts/builddb_rest_start.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Ensure an up-to-date (unmodified) product-metadata checkout
+# is available
+cd /home/couchbase
+rm -rf product-metadata
+git clone git://github.com/couchbase/product-metadata > /dev/null
+
+# Start pserve (as new, long-running, foreground process)
+[[ "$1" == "default" ]] && {
+    exec /usr/local/bin/pserve /etc/builddb_rest.ini
+}
+
+exec "$@"
