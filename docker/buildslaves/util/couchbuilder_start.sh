f432547a0735f3f2c07cd11e4086c7f72fd9b6ed
Chris Hillery <ceej@couchbase.com>
2018-10-11 01:04:49 -0700

    CBD-2635, CBD-2668: Add GPG keys and Gerrit user config to Swarm slaves
    Change-Id: I207585c238de73283f9cceda8781d9678eebba73
    Reviewed-on: http://review.couchbase.org/100494
    Reviewed-by: Kenneth Lareau <kenneth.lareau@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker/buildslaves/util/couchbuilder_start.sh
+++ docker/buildslaves/util/couchbuilder_start.sh
@@ -15,6 +15,10 @@
 #
 #   /run/secrets/jenkins_master_username
 #   /run/secrets/jenkins_master_password
+#
+# Finally, if any files with a .gpgkey extension exist in /run/secrets
+# (and the gpg command is available), those files will be imported into
+# the couchbase user's gpg keychain.
 
 # Hooks for build image-specific steps
 shopt -s nullglob
@@ -22,6 +26,15 @@ for hook in /usr/sbin/couchhook.d/*
 do
     "${hook}"
 done
+
+# Check for GPG keys
+command -v gpg >/dev/null 2>&1 && {
+    for gpgkey in /run/secrets/*.gpgkey
+    do
+        echo Importing ${gpgkey} ...
+        gpg --import ${gpgkey}
+    done
+}
 
 # if first argument is "swarm", run the (Jenkins) swarm jar with any arguments
 [[ "$1" == "swarm" ]] && {
