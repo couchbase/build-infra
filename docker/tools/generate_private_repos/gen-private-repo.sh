6417ed8d5fc71f54169fa275d8214c41009d96e6
Kenneth Lareau <kenneth.lareau@couchbase.com>
2019-02-07 16:15:47 -0800

    CBD-2772: Corrections for Docker image for generate_private_repos
    Change-Id: If0b7ce1c58fe13e817147a10a6fe30e300bd0d36
    Reviewed-on: http://review.couchbase.org/104619
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kenneth Lareau <kenneth.lareau@couchbase.com>

--- docker/tools/generate_private_repos/gen-private-repo.sh
+++ docker/tools/generate_private_repos/gen-private-repo.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env bash
 # Wrapper script to generate private repos from release manifest
 
+# Ensure an up-to-date (unmodified) manifest checkout is available
 WORK_DIR=/home/couchbase
-cd ${WORK_DIR}/manifest
-sudo git pull
+cd ${WORK_DIR}
+rm -rf manifest
+git clone git://github.com/couchbase/manifest > /dev/null
 
-sudo cp /etc/credentials.json ${WORK_DIR}/build-tools/generate_private_repos/
-sudo cp /etc/settings.yaml ${WORK_DIR}/build-tools/generate_private_repos/
+# Run generate_private_repos program
+[[ "$1" == "default" ]] && {
+    exec generate_private_repos --input ${WORK_DIR}/manifest/${MANIFEST} --release ${RELEASE} --conf /etc/projects.ini --folder-id ${GOOGLE_FOLDER_ID} || exit 1
+}
 
-cd ${WORK_DIR}/build-tools/generate_private_repos
-sudo ./gen-private-repos.py --input ${WORK_DIR}/manifest/${MANIFEST} --release ${RELEASE} --conf  ${WORK_DIR}/build-tools/generate_private_repos/projects.ini --folder-id ${GOOGLE_FOLDER_ID} || exit 1
+exec "$@"
