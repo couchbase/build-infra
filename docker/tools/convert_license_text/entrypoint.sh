2e598aa1e5687f3a54aae116c2e65468c048f236
Chris Hillery <ceej@couchbase.com>
2019-06-07 01:29:51 -0700

    MB-34293: Improvements to convert_license_text
     - Create license-long-lines.txt (same as input but with UTF-8 removed)
     - Keep input newlines in license.txt for better formatting
     - Add HINTS section to help message
    
    Change-Id: I1885f49ac305b82926265f0a02b3aea0bcf8c2b4
    Reviewed-on: http://review.couchbase.org/110343
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Kim Trang <kim.trang@couchbase.com>

--- docker/tools/convert_license_text/entrypoint.sh
+++ docker/tools/convert_license_text/entrypoint.sh
@@ -7,9 +7,10 @@ if [ ! -d /txt -o $# = 0 ]; then
 Converts a text file (which may have long text lines) to several forms:
 
   license.txt - wrapped to 80-column lines (without breaking URLs)
+  license-long-lines.txt - same formatting as input except for UTF-8 removal
   license.html - formatted as HTML, with certain lines made into headers
 
-In both cases, UTF-8 character sequences (such as double-quotes) will be
+In all cases, UTF-8 character sequences (such as double-quotes) will be
 replaced with their ASCII equivalents.
 
 The purpose of this container is to automate the conversion of License texts
@@ -18,12 +19,18 @@ into the various necessary forms.
 Usage:
 
   docker run --rm -v $(pwd):/txt couchbasebuild/convert_license_text
-     <file> [ <output text file> [ <output html file> ] ]
+     <input file> [ <text file> [ <long-line text file [ <html file> ] ] ]
 
-where <file> is a text file in the current directory. The output files will
+where <input file> is a text file in the current directory. The output files will
 also be placed in the current directory - don't pass absolute paths. If
-output filenames aren't specified, "license.txt" and "license.html" will be
-used.
+output filenames aren't specified, "license.txt", "license-long-lines.txt", and
+"license.html" will be used.
+
+HINTS:
+
+ - Ensure the input document has no trailing whitespace on lines.
+ - Ensure consistent formatting throughout input document; eg. all sections
+   should start with "XX. Section Title" and a newline before section body.
 
 EOF
     exit 1
@@ -34,27 +41,33 @@ cd /txt
 # Arguments.
 i=$1
 txt=${2-license.txt}
-html=${3-license.html}
+longtxt=${3-license-long-lines.txt}
+html=${4-license.html}
+
+tmpfile=/tmp/ascii.txt
 
 # Ok, we're really doing this. First strip UTF-8 stuff.
-iconv -f utf-8 -t ascii//TRANSLIT $i > /tmp/ascii.txt
+iconv -f utf-8 -t ascii//TRANSLIT $i > ${tmpfile}
 
-# Produce wrapped output.
-fmt /tmp/ascii.txt > $txt
+# Produce wrapped output. Use --split-only to honor newlines in source.
+fmt --split-only ${tmpfile} > ${txt}
+
+# Produce unwrapped output - just the de-UTF-8'd file.
+cp ${tmpfile} ${longtxt}
 
 # Tweak to prevent txt2html from scrambling things that look like lists.
-perl -pi -e 's/([A-Z1-9])\. /\1FROBOZ /g' /tmp/ascii.txt
+perl -pi -e 's/([A-Z1-9])\. /\1FROBOZ /g' ${tmpfile}
 
 # Convert to HTML.
 txt2html --titlefirst --outfile $html \
   --custom_heading_regexp 'License Agreement($| for the Java)' \
   --custom_heading_regexp 'SUPPLEMENTAL LICENSE TERMS$' \
-  /tmp/ascii.txt
+  ${tmpfile}
 
 # Un-tweak.
-perl -pi -e 's/([A-Z1-9])FROBOZ /\1. /g' $html
+perl -pi -e 's/([A-Z1-9])FROBOZ /\1. /g' ${html}
 
 # Change owner of output files (don't want them owned by root).
 uid=$(stat -c %u .)
 gid=$(stat -c %u .)
-chown $uid:$gid $txt $html
+chown $uid:$gid ${txt} ${longtxt} ${html}
