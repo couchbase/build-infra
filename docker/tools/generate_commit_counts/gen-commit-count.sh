8d612fb0d823f358c68682b8ce081ad4e48ae6f3
Kim Trang <kim.trang@couchbase.com>
2019-07-01 17:55:02 -0700

    CBD-2957 Dump content of email.txt to console output
    Change-Id: I050e87c109a1f9602b5f82f10658692c2e39d386
    Reviewed-on: http://review.couchbase.org/111464
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Kim Trang <kim.trang@couchbase.com>

--- docker/tools/generate_commit_counts/gen-commit-count.sh
+++ docker/tools/generate_commit_counts/gen-commit-count.sh
@@ -9,3 +9,7 @@ git clone git://github.com/couchbase/build-tools > /dev/null
 
 # Run generate_private_repos program
 python3.6  ${WORK_DIR}/build-tools/generate_commit_counts/gen-commit-counts.py --conf ${WORK_DIR}/build-tools/generate_commit_counts/projects.ini  --gerrit-config /etc/patch_via_gerrit.ini --git-config /etc/git_committer.ini --date-range ${DATE_RANGE} --recipient ${EMAIL_RECIPIENT} || exit 1
+
+# Dump content of email.txt to console
+echo -e "\n\n"
+cat email.txt
