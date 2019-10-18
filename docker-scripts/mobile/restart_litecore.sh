f60a4573350f7a0c8b2d272c5b3e7ee9c0539d5e
Chris Hillery <ceej@couchbase.com>
2019-10-18 01:21:44 -0700

    CBD-3050: Centos6-based slave for couchbase-lite-core
    Change-Id: I29a21b9c456f5356928ced7c4eae980211482b55
    Reviewed-on: http://review.couchbase.org/116323
    Reviewed-by: Jim Borden <jim.borden@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker-scripts/mobile/restart_litecore.sh
+++ docker-scripts/mobile/restart_litecore.sh
@@ -10,3 +10,4 @@ echo @@@@@@@@@@@@@@@@@@@@@@
 echo @ Recreating slaves
 echo @@@@@@@@@@@@@@@@@@@@@@
 ${SCRIPTPATH}/../restart_jenkinsdocker.py couchbasebuild/centos-72-litecore-build:20190926 mobile-litecore-linux 6501 mobile.jenkins.couchbase.com
+${SCRIPTPATH}/../restart_jenkinsdocker.py couchbasebuild/centos-69-litecore-build:20191016 mobile-litecore-centos6 6505 mobile.jenkins.couchbase.com
