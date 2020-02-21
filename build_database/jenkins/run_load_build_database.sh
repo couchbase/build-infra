aae0de8b37bb1a7beb01d15225d6f90bcada7ee2
Chris Hillery <ceej@couchbase.com>
2020-02-21 03:09:01 -0800

    CBD-3247: Mount /buildteam for build_db_loader
    Change-Id: Iaa48f19d045b8e55fbb04cf29e497af2269ae993
    Reviewed-on: http://review.couchbase.org/122653
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- build_database/jenkins/run_load_build_database.sh
+++ build_database/jenkins/run_load_build_database.sh
@@ -2,14 +2,14 @@
 
 docker pull couchbasebuild/load-build-database
 docker run --rm -u couchbase \
-    -v /builds:/builds \
+    -v /buildteam:/buildteam \
     -v /home/couchbase/jenkinsdocker-ssh:/home/couchbase/.ssh \
     -v /home/couchbase/build_database/build_db_loader_conf.ini:/etc/build_db_loader_conf.ini \
     -v /home/couchbase/build_database:/home/couchbase/build_database \
     couchbasebuild/load-build-database
 
 docker run --rm -u couchbase \
-    -v /builds:/builds \
+    -v /buildteam:/buildteam \
     -v /home/couchbase/build_database/build_db_loader_conf.ini:/etc/build_db_loader_conf.ini \
     -v /home/couchbase/build_database/jira.netrc:/home/couchbase/.netrc \
     couchbasebuild/load-build-database \
