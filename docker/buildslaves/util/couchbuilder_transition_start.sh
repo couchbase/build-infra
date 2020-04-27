0755aa89cd5687954b88b8e7fd5c2534f2753cd3
udkyo <blair.watt@couchbase.com>
2020-04-27 10:56:23 +0100

    CBD-3266: Set up pulling of config & secrets from central repository
    This change creates a new profiledata container, and profiledata stack
    (with profiledata_storage service) which will present config+secrets from
    swarm for build systems to *pull* at boot time. It also enables a maven
    cache across the couchbase-server macos + linux build/CV systems
    
    By pulling config+secrets in this way, various problems are addressed:
    - build systems needing to be redeployed for simple config file changes
    - no single source of truth for config+secrets
    - manual/awkward disaster recovery
    - deployment OS incompatibilities (e.g. unable to create /home/x on macos)
    - uncontrolled growth in security footprint
    
    Change-Id: Id297ff1f13081f007d99e93bf1e0dd52b606a30e
    Reviewed-on: http://review.couchbase.org/c/build-infra/+/126311
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Blair Watt <blair.watt@couchbase.com>

--- docker/buildslaves/util/couchbuilder_transition_start.sh
+++ docker/buildslaves/util/couchbuilder_transition_start.sh
@@ -14,20 +14,58 @@
 #   JENKINS_SLAVE_NAME (base name; will have container ID appended)
 #   JENKINS_SLAVE_LABELS
 #
-# In addition it expects the following Docker secrets to exist:
+# The following environment variables are also used to compose the
+# path profile data will be synchronised from
 #
-#   /run/secrets/jenkins_master_username
-#   /run/secrets/jenkins_master_password
+#   NODE_CLASS   (e.g. build, cv)
+#   NODE_PRODUCT (e.g. couchbase-server)
+#
+# In addition it expects the following Docker secret to exist:
+#
+#   /run/secrets/profile_ssh_key
 
-# First, copy any files in /ssh to /home/couchbase/.ssh, changing ownership to
-# user couchbase and maintaining permissions. This is for "old-school" slaves
-# which aren't using Docker Swarm secrets exclusively yet.
-if [ -d /ssh ] && [ "$(ls -A /ssh)" ]
+# We need to ensure these env vars are available in the exported function, and the script string which is evaled or execed via su
+export profile_port="4000"
+export profile_host="profiledata.build.couchbase.com"
+
+add_hostkeys() {
+    hostkeys="$(ssh-keyscan -p ${profile_port} ${profile_host})"
+    for key in "$hostkeys"
+    do
+      if ! cat /home/couchbase/.ssh/known_hosts 2>/dev/null | grep "$key"
+      then
+        echo "$key" >> /home/couchbase/.ssh/known_hosts
+      fi
+    done
+}
+
+export -f add_hostkeys
+
+if [ -f /run/secrets/profile_sync -a ! -e "${NODE_CLASS}" -a ! -e "${NODE_PRODUCT}" ]
 then
-    cp -a /ssh/* /home/couchbase/.ssh
+  echo "###########################"
+  echo "# Populating profile data #"
+  echo "###########################"
+
+  chmod 600 /run/secrets/profile_sync
+
+  # Ensure the host where the profile data lives is in our known_hosts before synchronisation. We also
+  # have to set permissions on directories here as we can only specify perms on files in the profile container
+  start_cmd="mkdir ~/.ssh \
+    && add_hostkeys \
+    && rsync --progress --archive --backup --executability --no-o --no-g -e \"ssh -p ${profile_port} -i /run/secrets/profile_sync\" couchbase@${profile_host}:${NODE_PRODUCT}/${NODE_CLASS}/linux/ /home/couchbase/ \
+    && ([ -d ~/.ssh ] && chmod 00700 ~/.ssh) \
+    && ([ -d ~/.gpg ] && chmod 00700 ~/.gpg)"
+
+  # we could concievably be running the container as root or couchbase, let's try
+  # to populate the profile data correctly either way
+  if [ "$(whoami)" = "couchbase" ]
+  then
+    eval $start_cmd
+  else
+    su couchbase -c "$start_cmd"
+  fi
 fi
-chown -R couchbase:couchbase /home/couchbase/.ssh
-chmod 600 /home/couchbase/.ssh/*
 
 # Hooks for build image-specific steps
 shopt -s nullglob
@@ -42,15 +80,18 @@ then
     /usr/sbin/couchhook.sh
 fi
 
-# Finally, if any files with a .gpgkey extension exist in /run/secrets
+# Finally, if any files exist in /home/couchbase/.gpg/
 # (and the gpg command is available), those files will be imported into
 # the couchbase user's gpg keychain.
+
 command -v gpg >/dev/null 2>&1 && {
-    for gpgkey in /run/secrets/*.gpgkey
+    shopt -s nullglob
+    for gpgkey in /home/couchbase/.gpg/* /run/secrets/*.gpgkey
     do
         echo Importing ${gpgkey} ...
         sudo -u couchbase -H gpg --import ${gpgkey}
     done
+    shopt -u nullglob
 }
 
 # if first argument is "swarm", run the (Jenkins) swarm jar with any arguments
---
+++ docker/tools/profiledata/README.md
@@ -0,0 +1,3 @@
+This support container provides an ssh daemon and rsync, to allow for clients to connect in and pull secrets, config files etc. at boot.
+
+Content is populated via docker stack configs and swarm secrets.
