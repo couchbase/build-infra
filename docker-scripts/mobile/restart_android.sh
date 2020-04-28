ec358e019240152627852b161492c24c66ced59d
Ming Ho <ming.ho@couchbase.com>
2020-04-28 10:15:50 -0700

    CBD-3378.  upgrade android build agents to ubuntu 18.0.4 -Ming Ho
    Change-Id: Ia8f3f200c2c2915856f8b643da6d23cb171f0e53
    Reviewed-on: http://review.couchbase.org/c/build-infra/+/126855
    Tested-by: Ming Ho <ming.ho@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- docker-scripts/mobile/restart_android.sh
+++ docker-scripts/mobile/restart_android.sh
@@ -6,8 +6,8 @@ pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd -P`
 popd > /dev/null
 
-${SCRIPTPATH}/../restart_jenkinsdocker.py --no-std-mounts --no-workspace couchbasebuild/ubuntu1604-mobile-lite-android:20191217 mobile-lite-android    6502 mobile.jenkins.couchbase.com
-${SCRIPTPATH}/../restart_jenkinsdocker.py --no-std-mounts --no-workspace couchbasebuild/ubuntu1604-mobile-lite-android:20191217 mobile-lite-android-02 6503 mobile.jenkins.couchbase.com
+${SCRIPTPATH}/../restart_jenkinsdocker.py --no-std-mounts --no-workspace couchbasebuild/ubuntu1804-mobile-lite-android:20191217 mobile-lite-android-01 6502 mobile.jenkins.couchbase.com
+${SCRIPTPATH}/../restart_jenkinsdocker.py --no-std-mounts --no-workspace couchbasebuild/ubuntu1804-mobile-lite-android:20191217 mobile-lite-android-02 6503 mobile.jenkins.couchbase.com
 ${SCRIPTPATH}/../restart_jenkinsdocker.py --no-std-mounts --no-workspace couchbasebuild/ubuntu1804-mobile-lite-android:20191217 mobile-lite-android-03 6504 mobile.jenkins.couchbase.com
 
 docker rm -f mobile-light
