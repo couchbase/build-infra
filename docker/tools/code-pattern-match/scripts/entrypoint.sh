e34df12a51438991a9dd36919c625a21dd645c9f
Blair Watt <blair.watt@couchbase.com>
2021-01-13 09:01:04 +0000

    CBD-3571: Code-pattern-match against source tarball
    Change-Id: I841ddcc7a48894b2de0139b35f8e312328793be0
    Reviewed-on: http://review.couchbase.org/c/build-infra/+/143258
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Blair Watt <blair.watt@couchbase.com>

--- docker/tools/code-pattern-match/scripts/entrypoint.sh
+++ docker/tools/code-pattern-match/scripts/entrypoint.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
 
-export MANIFEST_URL=${MANIFEST_URL:-git://github.com/couchbase/manifest}
-export MANIFEST_FILE=${MANIFEST_FILE:-branch-master.xml}
-export MANIFEST_GROUPS=${MANIFEST_GROUPS:-all}
-
 set -e
 
 if [ "$1" != "" ]; then
     exec "$@"
 else
-    set -x && repo init -u "${MANIFEST_URL}" -m "${MANIFEST_FILE}" -g "${MANIFEST_GROUPS}"
-    set +x
-    repo sync -j8
-    echo
+    if [ "$SOURCE_URL" != "" ]
+    then
+        echo "Downloading source tarball..."
+        curl "$SOURCE_URL" | tar -xz
+    else
+        echo "Starting repo sync..."
+        export MANIFEST_URL=${MANIFEST_URL:-git://github.com/couchbase/manifest}
+        export MANIFEST_BRANCH=${MANIFEST_BRANCH:-master}
+        export MANIFEST_FILE=${MANIFEST_FILE:-branch-master.xml}
+        export MANIFEST_GROUPS=${MANIFEST_GROUPS:-all}
+        set -x && repo init -u "${MANIFEST_URL}" -b "${MANIFEST_BRANCH}" -m "${MANIFEST_FILE}" -g "${MANIFEST_GROUPS}"
+        set +x
+        repo sync -j8
+        echo
+    fi
 
     if [ "$1" = "" ]; then
         exec find-patterns
