0aba905352ce7a30a7e1cb19be42259f1e9b44ad
Blair Watt <blair.watt@couchbase.com>
2021-05-07 16:20:12 +0100

    CBD-4087: Add cygwin playbook. Add components to msvc2017 role
    The Win 8.1 SDK and Universal CRT SDK components are required
    to build jemalloc on msvc2017
    
    Change-Id: Ice4c865906943d11137b178818d634949e844a32
    Reviewed-on: http://review.couchbase.org/c/build-infra/+/152934
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>
    Tested-by: Blair Watt <blair.watt@couchbase.com>

---
+++ ansible/windows/roles/cygwin/files/install_cygwin.bat
@@ -0,0 +1,36 @@
+@ECHO OFF
+REM -- Automates cygwin installation
+REM -- Based on: https://github.com/rtwolf/cygwin-auto-install
+
+SETLOCAL
+
+REM -- Change to the directory of the executing batch file
+CD %~dp0
+
+REM -- Configure our paths
+SET SITE=http://mirrors.xmission.com/cygwin/
+SET LOCALDIR="%USERPROFILE%\Downloads"
+SET ROOTDIR=C:\cygwin
+
+REM -- These are the packages we will install (in addition
+REM -- to the default packages)
+SET PACKAGES=autoconf,autogen,autoconf,gawk,grep,make,sed,wget,automake,wget,dos2unix,sh
+
+REM -- Do the install!
+ECHO *** INSTALLING DEFAULT PACKAGES
+C:\ansible_files\cygwin_setup --quiet-mode --no-desktop --download --local-install --no-verify -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%"
+ECHO.
+ECHO.
+ECHO *** INSTALLING CUSTOM PACKAGES
+C:\ansible_files\cygwin_setup -q -d -D -L -X -s %SITE% -l "%LOCALDIR%" -R "%ROOTDIR%" -P %PACKAGES%
+
+REM -- Show what we did
+ECHO.
+ECHO.
+ECHO cygwin installation updated
+ECHO  - %PACKAGES%
+ECHO.
+
+ENDLOCAL
+
+EXIT /B 0
