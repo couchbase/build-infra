4d0a451d6b3323677b24e86fc564e06178ca2ccf
Chris Hillery <ceej@couchbase.com>
2022-04-06 20:14:04 -0700

    CBD-4655, CBD-4653: Removing old-OS agents running on mega2
    - mobile centos6 agents removed outright
    - mobile ubuntu16 agent moved to mega3
    - server debian9/ubuntu14 agents removed outright
    - mobile clamav agent removed outright (using clamav job on
      server.jenkins now)
    
    Change-Id: I275491a6421391886b9a6e21c34051848fdfb12b
    Reviewed-on: https://review.couchbase.org/c/build-infra/+/173392
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

--- docker-scripts/mobile/restart_sgw_jenkins.sh
+++ docker-scripts/mobile/restart_sgw_jenkins.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-# These are currently all hosted on mega2
+# These are currently all hosted on mega3
 
 pushd `dirname $0` > /dev/null
 SCRIPTPATH=`pwd -P`
 popd > /dev/null
 
 ${SCRIPTPATH}/../restart_jenkinsdocker.py --no-std-mounts --no-workspace couchbasebuild/ubuntu1604-sgw-build:20181204 mobile-sgw-ubuntu16-01 2323 mobile.jenkins.couchbase.com
-${SCRIPTPATH}/../restart_jenkinsdocker.py --no-std-mounts --no-workspace couchbasebuild/centos6-sgw-build:20190926 mobile-sgw-centos6-01 2320 mobile.jenkins.couchbase.com
