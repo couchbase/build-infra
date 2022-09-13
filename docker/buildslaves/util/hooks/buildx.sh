a83538d87518c54f1adf164f575b2e6d414e4d38
Couchbase Build Team <build-team@couchbase.com>
2022-09-13 09:11:45 +0100

    CBD-5109: Use buildx-arm64-01 hostname in configs/hook
    Change-Id: I1845ec6d5aeb11f756703e8a26924b7851e92209
    Reviewed-on: https://review.couchbase.org/c/build-infra/+/179854
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Blair Watt <blair.watt@couchbase.com>

--- docker/buildslaves/util/hooks/buildx.sh
+++ docker/buildslaves/util/hooks/buildx.sh
@@ -3,17 +3,16 @@
 # This hook is used to ensure we are using a multi-arch capable builder
 
 sudo -u couchbase --set-home --preserve-env \
-  env -u jenkins_user -u jenkins_password -u SUDO_UID -u SUDO_GID -u SUDO_USER -u SUDO_COMMAND \
   bash -c '
   set -ex;
   test -d ~/.ssh || mkdir ~/.ssh;
-  test -f "~/.ssh/known_hosts" && ssh-keygen -R 10.100.151.12;
-  ssh-keyscan -H 10.100.151.12 >> ~/.ssh/known_hosts;
+  test -f "~/.ssh/known_hosts" && ssh-keygen -R buildx-arm64-01.build.couchbase.com;
+  ssh-keyscan -H buildx-arm64-01.build.couchbase.com >> ~/.ssh/known_hosts;
   if docker buildx ls | awk "{print $1}" | tail -n +1 | grep cbmultiarch;
   then
     docker buildx rm cbmultiarch;
   fi;
   docker buildx create --name cbmultiarch --driver docker-container --use --platform linux/amd64;
-  docker buildx create --name cbmultiarch --driver docker-container --use --append ssh://buildx-arm64 --platform linux/arm64;
+  docker buildx create --name cbmultiarch --driver docker-container --use --append ssh://buildx-arm64-01 --platform linux/arm64;
   docker buildx inspect --bootstrap;
 '
