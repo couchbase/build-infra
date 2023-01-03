a4e304f1e8cf3528f97ee27d5880b4c1c9cd844e
Blair Watt <blair.watt@couchbase.com>
2023-01-03 14:43:54 +0000

    CBD-5222: Add priv key scanning, improve aws secret scanning
    Change-Id: I0bbcdb951a6d756c335a0ca8ce85c399901fb7cd
    Reviewed-on: https://review.couchbase.org/c/build-infra/+/183626
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>
    Tested-by: Blair Watt <blair.watt@couchbase.com>

--- docker/tools/code-pattern-match/scripts/find-patterns.js
+++ docker/tools/code-pattern-match/scripts/find-patterns.js
@@ -11,35 +11,47 @@ let exitCode = 0
 // If we've been passed an argument, treat it as the directory we intend to scan
 if (Deno?.args?.[0]) Deno.chdir(Deno.args[0])
 
-async function getJson(filename) {
-    return JSON.parse(await Deno.readTextFile(filename))
-}
+const getJson = async filename => JSON.parse(await Deno.readTextFile(filename))
+const heading = text => console.log(`${text}\n${'-'.repeat(text.length)}`)
 
 const patterns = await getJson('/patterns.json')
 
 // Walk through list of patterns running ag against them and treating successes
 // (no matches) and failures (matches) accordingly
 for (let i = 0; i < patterns?.length; i++) {
-    const outstr = `Checking pattern: ${patterns[i].name}`
-    console.log(`${outstr}\n${'-'.repeat(outstr.length)}`)
-    const exclude = patterns[i]?.exclusions ? `(?!${patterns[i].exclusions.join('|')})` : ''
-    const pattern = patterns[i].pattern.replace("@EXCLUSIONS@", exclude)
-    const p = Deno.run({
-        cmd: ['ag', '--parallel', pattern]
-    })
-    const { code } = await p.status()
-    p.close()
+    // patterns are enabled by default
+    const enabled = !patterns[i].hasOwnProperty("enabled") || patterns[i].enabled
+    // whole words are matched by default
+    const wordMatch = !patterns[i].hasOwnProperty("word_match") || patterns[i].word_match;
+    if (enabled) {
+        heading(`Checking pattern: ${patterns[i].name}`)
+        const exclude = patterns[i]?.exclusions ? `(?!${patterns[i].exclusions.join('|')})` : ''
+        const pattern = patterns[i].pattern.replace("@EXCLUSIONS@", exclude)
+        const args = ['ag', wordMatch ? '-w' : '', '-W200'].filter(arg => arg)
+        for(const filePattern in patterns[i].whitelist) {
+            args.push('--ignore')
+            args.push(`${patterns[i].whitelist[filePattern]}`)
+        }
+        args.push(pattern)
+        const p = Deno.run({
+            cmd: args
+        })
+        const { code } = await p.status()
+        p.close()
 
-    if (code === 0) {
-        // match
-        console.log("WARNING: matches found")
-        console.log("Remediation:", patterns[i].remediation)
-        console.log()
-        exitCode = 1
+        if (code === 0) {
+            // match
+            console.log("WARNING: matches found")
+            console.log("Remediation:", patterns[i].remediation)
+            console.log()
+            exitCode = 1
+        } else {
+            // no match
+            console.log("No matches")
+            console.log()
+        }
     } else {
-        // no match
-        console.log("No matches")
-        console.log()
+        console.log(`Skipped disabled pattern: ${patterns[i].name}`)
     }
 }
 
