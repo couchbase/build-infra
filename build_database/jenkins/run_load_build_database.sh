decef8b22e756bf5ae38ec01ef58ffb4d8b513ea
Chris Hillery <ceej@couchbase.com>
2023-04-25 14:36:32 -0700

    CBD-5393: Switch to couchbase/python-couchbase-commons
    Unintentionally submitted pointing to my personal code fork.
    
    Change-Id: I61e09dda536dd9bf34430f43bd8cdcb0434fd9f1
    Reviewed-on: https://review.couchbase.org/c/build-infra/+/190138
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- build_database/jenkins/run_load_build_database.sh
+++ build_database/jenkins/run_load_build_database.sh
@@ -5,12 +5,12 @@ docker run --pull always --rm -u couchbase \
     -v /home/couchbase/jenkinsdocker-ssh:/home/couchbase/.ssh \
     -v /home/couchbase/build_database/build_db_loader_conf.ini:/etc/build_db_loader_conf.ini \
     -v /home/couchbase/build_database:/home/couchbase/build_database \
-    couchbasebuild/load-build-database:20230414 \
+    couchbasebuild/load-build-database:20230425 \
     load_build_database -c /etc/build_db_loader_conf.ini -d
 
 docker run --rm -u couchbase \
     -v /buildteam:/buildteam \
     -v /home/couchbase/build_database/build_db_loader_conf.ini:/etc/build_db_loader_conf.ini \
     -v /home/couchbase/build_database/jira.netrc:/home/couchbase/.netrc \
-    couchbasebuild/load-build-database:20230414 \
+    couchbasebuild/load-build-database:20230425 \
     jira_commenter -c /etc/build_db_loader_conf.ini -d
