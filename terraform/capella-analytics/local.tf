da68f29f478d51bda05ea87e66a945ee63bf4e5d
Ming Ho <ming.ho@couchbase.com>
2023-08-29 09:58:47 -0700

    CBD-5578, remove account info.
    -Ming
    
    Change-Id: I8cefecbda21fe6eb3915637fc6a0eb6f079f74f2
    Reviewed-on: https://review.couchbase.org/c/build-infra/+/196298
    Tested-by: Ming Ho <ming.ho@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- terraform/capella-analytics/local.tf
+++ terraform/capella-analytics/local.tf
@@ -1,27 +1,19 @@
 data "aws_caller_identity" "current" {}
 
-# aws accounts:
-#   Capella sbx env (dbaas5): 264138468394
-#   cb-qe: 516524556673
-#   cb-perf: 955582452726
-
-
 locals {
   repositories = ["capella-analytics", "goldfish-nebula"]
   aws_region             = "us-east-2"
   ecr_image_scan         = true
+  # Populate IPs that should be given access to
+  # before running terraform
   couchbase_ips          = [
-    "12.145.26.240/29",
-    "12.235.169.89/30",
-    "32.142.206.162/30",
-    "64.124.71.194/29",
-    "67.212.150.204/27"
+    "xx.xxx.xxx.xx/29"
   ]
+
+  # Populate AWS accounts that should be granted access first by adding
+  # "arn:aws:iam::xxxxxxxxxxxx:root"
   repo_pull_access_arns  = [
     "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root",
-    "arn:aws:iam::264138468394:root",
-    "arn:aws:iam::516524556673:root",
-    "arn:aws:iam::955582452726:root"
   ]
   repo_push_access_arns  = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
   repo_push_access_roles = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/cbd-4108_server_jenkins_worker"]
