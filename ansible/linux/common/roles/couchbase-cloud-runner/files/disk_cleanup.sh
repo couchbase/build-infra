5509dd1d664fee5c71c7bb8f51a8a9738b3f7e16
Blair Watt <blair.watt@couchbase.com>
2024-03-04 11:39:20 +0000

    CBD-5821: More aggressive disk cleanup on GHA workers
    Change-Id: I7a086f7cb20bb0a9ea2c8c545a378310593cdc61
    Reviewed-on: https://review.couchbase.org/c/build-infra/+/206647
    Reviewed-by: Chris Hillery <ceej@couchbase.com>
    Tested-by: Blair Watt <blair.watt@couchbase.com>

--- ansible/linux/common/roles/couchbase-cloud-runner/files/disk_cleanup.sh
+++ ansible/linux/common/roles/couchbase-cloud-runner/files/disk_cleanup.sh
@@ -4,32 +4,32 @@
 ## cached in .cache/go-build.  Diskspace fills up quickly.  We need
 ## remove the directory to free up the diskspace.
 used_percent=$(df -kh . | tail -n1 | awk '{print $5}' |sed -e 's/%//')
-if [[ $(($used_percent)) -ge 90 ]]; then
-  echo -e "$(date) \tDisk usage is above 90% - removing go-build cache"
+if [[ $(($used_percent)) -ge 85 ]]; then
+  echo -e "$(date) \tDisk usage is above 85% - removing go-build cache"
   rm -rf /home/couchbase/.cache/go-build
 fi
 
 # If things are still bad, run docker system prune
 used_percent=$(df -kh . | tail -n1 | awk '{print $5}' |sed -e 's/%//')
-if [[ $(($used_percent)) -ge 90 ]]; then
-  echo -e "$(date) \tDisk usage is above 90% - running docker system prune"
+if [[ $(($used_percent)) -ge 85 ]]; then
+  echo -e "$(date) \tDisk usage is above 85% - running docker system prune"
   docker system prune --force
 fi
 
 # Still bad, run docker system prune --all to clear build cache
 used_percent=$(df -kh . | tail -n1 | awk '{print $5}' |sed -e 's/%//')
-if [[ $(($used_percent)) -ge 90 ]]; then
-  echo -e "$(date) \tDisk usage is above 90% - running docker system prune --all"
+if [[ $(($used_percent)) -ge 85 ]]; then
+  echo -e "$(date) \tDisk usage is above 85% - running docker system prune --all"
   docker system prune --all --volumes --force
 fi
 
 # Still bad, the hammer: bazel clean --expunge
 used_percent=$(df -kh . | tail -n1 | awk '{print $5}' |sed -e 's/%//')
-if [[ $(($used_percent)) -ge 90 ]]; then
+if [[ $(($used_percent)) -ge 85 ]]; then
   if [ -d /opt/gha/_work/couchbase-cloud/couchbase-cloud ]; then
-    echo -e "$(date) \tDisk usage is above 90% - running bazel clean --expunge"
+    echo -e "$(date) \tDisk usage is above 85% - running bazel clean --expunge"
     cd /opt/gha/_work/couchbase-cloud/couchbase-cloud
     /home/couchbase/go/bin/bazel clean --expunge
-    mkdir -p $(bazel info output_path)
+    mkdir -p $(/home/couchbase/go/bin/bazel info output_path)
   fi
 fi
