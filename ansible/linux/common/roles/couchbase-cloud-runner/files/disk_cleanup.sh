c9575f884b7df34fb4483678ba0e3f437b99cab7
Chris Hillery <ceej@couchbase.com>
2024-03-11 17:22:31 -0700

    Don't attempt to clean up go-build cache
    This caused a number of GHA build failures, apparently due to the
    cleanup script running while a job was in progress.
    
    Change-Id: I2729c10f12b0215e42f55b94832ae39c2a709f6c
    Reviewed-on: https://review.couchbase.org/c/build-infra/+/207046
    Tested-by: Chris Hillery <ceej@couchbase.com>
    Reviewed-by: Blair Watt <blair.watt@couchbase.com>

--- ansible/linux/common/roles/couchbase-cloud-runner/files/disk_cleanup.sh
+++ ansible/linux/common/roles/couchbase-cloud-runner/files/disk_cleanup.sh
@@ -1,33 +1,27 @@
 #!/bin/bash
 
-## during golang upgrade, multiple version of golang's modules are
-## cached in .cache/go-build.  Diskspace fills up quickly.  We need
-## remove the directory to free up the diskspace.
-used_percent=$(df -kh . | tail -n1 | awk '{print $5}' |sed -e 's/%//')
-if [[ $(($used_percent)) -ge 85 ]]; then
-  echo -e "$(date) \tDisk usage is above 85% - removing go-build cache"
-  rm -rf /home/couchbase/.cache/go-build
-fi
+# Disk free threshold - above 85%, try cleaning things
+DF_THRESHOLD=85
 
-# If things are still bad, run docker system prune
+# If disk free percentage is too low, try docker prune
 used_percent=$(df -kh . | tail -n1 | awk '{print $5}' |sed -e 's/%//')
-if [[ $(($used_percent)) -ge 85 ]]; then
-  echo -e "$(date) \tDisk usage is above 85% - running docker system prune"
+if [[ $(($used_percent)) -ge ${DF_THRESHOLD} ]]; then
+  echo -e "$(date) \tDisk usage is above ${DF_THRESHOLD}% - running docker system prune"
   docker system prune --force
 fi
 
 # Still bad, run docker system prune --all to clear build cache
 used_percent=$(df -kh . | tail -n1 | awk '{print $5}' |sed -e 's/%//')
-if [[ $(($used_percent)) -ge 85 ]]; then
-  echo -e "$(date) \tDisk usage is above 85% - running docker system prune --all"
+if [[ $(($used_percent)) -ge ${DF_THRESHOLD} ]]; then
+  echo -e "$(date) \tDisk usage is above ${DF_THRESHOLD}% - running docker system prune --all"
   docker system prune --all --volumes --force
 fi
 
 # Still bad, the hammer: bazel clean --expunge
 used_percent=$(df -kh . | tail -n1 | awk '{print $5}' |sed -e 's/%//')
-if [[ $(($used_percent)) -ge 85 ]]; then
+if [[ $(($used_percent)) -ge ${DF_THRESHOLD} ]]; then
   if [ -d /opt/gha/_work/couchbase-cloud/couchbase-cloud ]; then
-    echo -e "$(date) \tDisk usage is above 85% - running bazel clean --expunge"
+    echo -e "$(date) \tDisk usage is above ${DF_THRESHOLD}% - running bazel clean --expunge"
     cd /opt/gha/_work/couchbase-cloud/couchbase-cloud
     /home/couchbase/go/bin/bazel clean --expunge
     mkdir -p $(/home/couchbase/go/bin/bazel info output_path)
