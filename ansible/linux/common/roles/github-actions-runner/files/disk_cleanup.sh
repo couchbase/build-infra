345a6b889ef4367931c7673e9bd4e88acea6c052
James Lee <james.lee@couchbase.com>
2024-03-26 13:30:44 +0000

    Prefer a "small-and-often" approach to disk clean-up
    Prior to CV taking place, we will now run a small handful of
    unconditional clean-up tasks:
    
    1. Removing temporary Go directories
    2. Cleaning up old docker images
    
    This is followed by conditional clean-up of increasing impact:
    
    1. Test/fuzz caches
    2. Module caches
    3. Build cache
    4. BAZEL clean
    
    Upon failing that, we fall-back to more disruptive measures:
    
    1. Clean-up of all Go caches
    2. Removal of Go build cache
    3. Full docker prune
    4. BAZEL expunge
    
    ```sh
    A job started hook has been configured by the self-hosted runner administrator
    Run '/home/couchbase/disk_cleanup.sh'
      shell: /usr/bin/bash --noprofile --norc -e -o pipefail {0}
    Error: Process completed with exit code 1.
    ```
    
    Now that we're not running this script via `cron`, I've also modified
    the logging to output to `stdout` - as well as a logfile - so that we
    have some output in GitHub Actions, which is something we ran into
    recently.
    
    Change-Id: I040985eb37f77a0f5048e48b136a080115d12fc7
    Reviewed-on: https://review.couchbase.org/c/build-infra/+/207698
    Tested-by: James Lee <james.lee@couchbase.com>
    Reviewed-by: Chris Hillery <ceej@couchbase.com>

--- ansible/linux/common/roles/github-actions-runner/files/disk_cleanup.sh
+++ ansible/linux/common/roles/github-actions-runner/files/disk_cleanup.sh
@@ -1,57 +1,105 @@
-#!/bin/bash
+#!/bin/bash -eu
+
+set -o pipefail
 
 # NOTE: This script is launched by GitHub Actions at the start of each
 # job, not cron. It is therefore guaranteed that no real build job is
 # executing, so it's safe to do things like deleting caches.
 
-# Log output of this script
-LOGFILE=/home/couchbase/disk_cleanup.log
-exec 3>&1 1>>${LOGFILE} 2>&1
+# Log to a file, so we have historical information.
+LOGFILE="${HOME}/disk_cleanup.log"
 
-# Disk free threshold check - above 80%, try cleaning things
-disk_too_full() {
-  used_percent=$(df -kh . | tail -n1 | awk '{print $5}' |sed -e 's/%//')
-  [[ $(($used_percent)) -ge 80 ]]
+# Returns the disk usage as a percentage (without the %).
+function usage() {
+  echo $(df -kh . | tail -n1 | awk '{print $5}' | sed -e 's/%//')
 }
 
-# If disk free percentage is too low, try docker prune
-if disk_too_full; then
-  echo -e "$(date) \trunning docker system prune"
-  docker system prune --force
-fi
+# Logs the given message to stdout and the logfile.
+function log() {
+  echo "$(date): $1" | tee -a ${LOGFILE}
+}
 
-# Next up, blow away Go build cache
-if disk_too_full; then
-  echo -e "$(date) \tdeleting go-build cache"
-  rm -rf ~/.cache/go-build
-fi
+# Runs the given command, displaying useful information before/after.
+function run() {
+  log "Started running \"$1\" | {\"usage\":\"$(usage)%\"}"
 
-# Still bad, run docker system prune --all --volumes to clear build cache
-if disk_too_full; then
-  echo -e "$(date) \trunning docker system prune --all --volumes --force"
-  docker system prune --all --volumes --force
-fi
+  eval $1
 
-# Finally, the hammer - bazel clean --expunge.
-if disk_too_full; then
-  # This would be created by GHA jobs at this path
-  BZL=/home/couchbase/go/bin/bazel
-  if [ -x ${BZL} ]; then
-    echo -e "$(date) \trunning bazel clean --expunge"
-    cd /opt/gha/_work/couchbase-cloud/couchbase-cloud
+  log "Completed running \"$1\" | {\"usage\":\"$(usage)%\"}"
+}
 
-    # Shut down any currently-running Bazel server (shouldn't be any)
-    ${BZL} shutdown
+# Runs the given Go command, note that you do not need to provide the path to Go (e.g. clean -modcache).
+function run_go() {
+  # Find a usable install of Go, preferably the most up-to-date one.
+  GO=$(find ${HOME}/install -name go -type f -executable | sort | tail -1)
 
-    # Run clean - this starts a new server but shuts it down afterwards
-    ${BZL} clean --expunge
-
-    # Start a new server to cause it to re-create the "external" directories
-    ${BZL} info output_path
-
-    # Shut it down again so that the first build job will create it fresh
-    ${BZL} shutdown
+  if [ ! -z ${GO} ]; then
+    return
   fi
+
+  run "${GO} $1"
+}
+
+# Runs the given Bazel command, note that you do not need to provide the path to Bazel (e.g. clean --expunge)
+function run_bazel() {
+  BAZEL="${HOME}/go/bin/bazel"
+
+  if [ ! -x ${BAZEL} ]; then
+    return
+  fi
+
+  run "${BAZEL} $1"
+}
+
+# Provide verbose information that will display the start/exit in GitHub Actions.
+log "Disk cleanup starting"
+trap "log 'Disk cleanup completed'" EXIT
+
+# Go builds using '/tmp' by default and often litters these directories around.
+run "rm -rf $(ls -d /tmp/go-build*)"
+
+# Clean up old docker images.
+run "docker system prune --filter 'until=6h' --force"
+
+# A high watermark, percentage of disk space used.
+THRESHOLD=75
+
+# Sacrifice the test caches first; given the low churn, 'remote-cache' does a very good job of this.
+if [[ $(usage) -gt ${THRESHOLD} ]]; then
+  run_go "clean -testcache -fuzzcache"
 fi
 
-echo -e "$(date) \tDisk cleanup completed."
+# Remove the module cache, which balloons with dependency upgrades.
+if [[ $(usage) -gt ${THRESHOLD} ]]; then
+  run_go "clean -modcache"
+fi
+
+# Finally, remove the build cache; that's the most useful to retain, if possible.
+if [[ $(usage) -gt ${THRESHOLD} ]]; then
+  run_go "clean -cache"
+fi
+
+# Perform a light Bazel clean, removing output and directories (but potentially leaving unknown targets).
+if [[ $(usage) -gt ${THRESHOLD} ]]; then
+  run_bazel "clean"
+fi
+
+# Completely remove any Go caches.
+if [[ $(usage) -gt ${THRESHOLD} ]]; then
+  run_go "clean -cache -testcache -modcache -fuzzcache"
+fi
+
+# If we were unable to find Go, delete some of the well known directories.
+if [[ $(usage) -gt ${THRESHOLD} ]]; then
+  run "rm -rf ${HOME}/.cache/go-build"
+fi
+
+# Prune everything possible from Docker.
+if [[ $(usage) -gt ${THRESHOLD} ]]; then
+  run "docker system prune --all --volumes --force"
+fi
+
+# Remove the Bazel cache, working tree and output files; this force Bazel's hand to clean-up everything.
+if [[ $(usage) -gt ${THRESHOLD} ]]; then
+  run_bazel "clean --expunge"
+fi
