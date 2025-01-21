531765386b5d1331fecadf3c2514615a51a915a3
Chris Hillery <ceej@couchbase.com>
2025-01-21 04:00:57 -0800

    CBD-6145: Script to back up Black Duck to SAN and S3
    Change-Id: I2079a13218eca64f103726acb16dd1f6ad7ca15c
    Reviewed-on: https://review.couchbase.org/c/build-infra/+/222181
    Reviewed-by: Blair Watt <blair.watt@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>
    Tested-by: Chris Hillery <ceej@couchbase.com>

---
+++ blackduck/backup_blackduck.sh
@@ -0,0 +1,48 @@
+#!/bin/bash -ex
+
+# Script to back up our Black Duck Hub data and configuration. Expected
+# to run on the Black Duck Hub server where Black Duck is installed in
+# `/home/couchbase/blackduck`, and also expects the `/buildteam`
+# mountpoint to be available.
+
+BD_ROOT=/home/couchbase/blackduck
+BACKUP_ROOT=/buildteam/backups/blackduck
+
+if [ ! -d ${BD_ROOT} ]; then
+    echo "Black Duck Hub not installed at ${BD_ROOT}"
+    exit 1
+fi
+
+if [ ! -d /buildteam ]; then
+    echo "Mountpoint /buildteam not available"
+    exit 1
+fi
+
+source ${BD_ROOT}/couchbase-blackduck-versions
+
+# Create backup directory
+BACKUP_DIR=${BACKUP_ROOT}/blackduck_couchbase_$(date +%Y%m%d%H%M)
+mkdir -p ${BACKUP_DIR}
+
+# Copy important local config files
+cp ${BD_ROOT}/couchbase* \
+    ${BD_ROOT}/start_hub.sh \
+    ${BACKUP_DIR}/
+
+# Create backup
+${BD_ROOT}/hub-${HUB_VERSION}/docker-swarm/bin/hub_create_data_dump.sh  \
+    ${BACKUP_DIR}/ 2>&1
+
+# Retain 60 backups
+pushd ${BACKUP_DIR}/..
+ls -t | tail -n +60 | xargs rm -rf >> /tmp/backup-blackduck.log
+
+# Sync backup directory to S3, but only if it's definitely mounted from
+# the NAS - since we have `--delete` here, we don't want to blow away
+# old data from S3 if the NAS isn't mounted and the older backups are
+# missing locally.
+if [ ! -e ${BACKUP_ROOT}/DONT_DELETE_THIS.txt ]; then
+    echo "NAS mountpoint not found, not syncing to S3!"
+    exit 1
+fi
+aws s3 sync --delete ${BACKUP_ROOT}/ s3://cb-blackduck.backups/
