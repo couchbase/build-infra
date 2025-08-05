3c91579bee83ddcf62352f242ede97a5052bc659
Chris Hillery <ceej@couchbase.com>
2025-08-05 01:02:25 -0700

    DFS-based couchbase-server build agent

---
+++ docker/buildslaves/couchbase-server/dfs/universal_entrypoint.sh
@@ -0,0 +1,26 @@
+#!/bin/bash -e
+#
+# Universal Entrypoint Script for Couchbase Build Containers
+# https://github.com/couchbase/build-infra/blob/master/docker/entrypoint
+
+# Note: these CB_ variables are used by the entrypoint plugin also, so
+# don't change the variable names.
+export CB_ENTRYPOINT_BASE=${CB_ENTRYPOINT_BASE:-https://raw.githubusercontent.com/couchbase/build-infra/refs/heads/master/docker/entrypoint}
+export CB_ENTRYPOINT_LOCAL_ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+export CB_ENTRYPOINT_PLUGIN_CACHE=/tmp/entrypoint-plugins
+
+# Manually download and invoke the real entrypoint plugin.
+rm -rf "${CB_ENTRYPOINT_PLUGIN_CACHE}"
+mkdir -p "${CB_ENTRYPOINT_PLUGIN_CACHE}/universal"
+plugpath=universal/entrypoint.sh
+
+if [ ! -z "${CB_DEBUG_LOCAL_PLUGINS}" ]; then
+    echo "Bootstrap from ${CB_ENTRYPOINT_LOCAL_ROOT}/plugins/${plugpath}"
+    cp "${CB_ENTRYPOINT_LOCAL_ROOT}/plugins/${plugpath}" ${CB_ENTRYPOINT_PLUGIN_CACHE}/${plugpath}
+else
+    echo "-- Bootstrap from ${CB_ENTRYPOINT_BASE}/plugins/${plugpath}"
+    curl -fsSL \
+        "${CB_ENTRYPOINT_BASE}/plugins/${plugpath}" \
+        > ${CB_ENTRYPOINT_PLUGIN_CACHE}/${plugpath}
+fi
+source ${CB_ENTRYPOINT_PLUGIN_CACHE}/${plugpath}
