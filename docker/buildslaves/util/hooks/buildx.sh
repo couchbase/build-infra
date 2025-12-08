f8cf9ae84bf7bf6daf1bbf66f7182ed8d7afc4d3
Blair Watt <blair.watt@couchbase.com>
2025-12-08 10:49:28 +0000

    CBD-6523: Add envsubst to zz-lightweight
    Change-Id: I8357200d85f631131f217fc61773d7ef7fd4614c
    Reviewed-on: https://review.couchbase.org/c/build-infra/+/237351
    Tested-by: Blair Watt <blair.watt@couchbase.com>
    Reviewed-by: Ming Ho <ming.ho@couchbase.com>

--- docker/buildslaves/util/hooks/buildx.sh
+++ docker/buildslaves/util/hooks/buildx.sh
@@ -8,14 +8,10 @@ if ! sudo -u couchbase find /var/run/docker.sock -writable ; then exit 0; fi
 sudo -u couchbase --set-home --preserve-env \
   bash -c '
   set -ex;
-  test -d ~/.ssh || mkdir ~/.ssh;
-  test -f "~/.ssh/known_hosts" && ssh-keygen -R buildx-arm64-01.build.couchbase.com;
-  ssh-keyscan -H buildx-arm64-01.build.couchbase.com >> ~/.ssh/known_hosts;
   if docker buildx ls | awk "{print $1}" | tail -n +1 | grep cbmultiarch;
   then
     docker buildx rm cbmultiarch;
   fi;
-  docker buildx create --name cbmultiarch --driver docker-container --use --platform linux/amd64;
-  docker buildx create --name cbmultiarch --driver docker-container --use --append ssh://buildx-arm64-01 --platform linux/arm64;
+  docker buildx create --name cbmultiarch --driver docker-container --use --platform linux/amd64,linux/arm64;
   docker buildx inspect --bootstrap;
 '
