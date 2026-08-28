9c540e46676e9602ae922f3cf48458aa537405b1
Ming Ho <ming.ho@couchbase.com>
2026-08-28 15:42:43 -0700

    CBD-6826, attempt to fix cross compile docker build error.
    bump tonistiigi/binfmt to qemu-v9.2.2
    lock moby/buildkit to v0.23.2, which bundles the same version of qemu.
    
    -Ming
    
    Change-Id: I2acf8d7bd3d5b69ce18142c6f005c3c3742ab571
    Reviewed-on: https://review.couchbase.org/c/build-infra/+/251952
    Reviewed-by: Blair Watt <blair.watt@couchbase.com>
    Tested-by: Ming Ho <ming.ho@couchbase.com>

--- docker/buildslaves/util/hooks/buildx.sh
+++ docker/buildslaves/util/hooks/buildx.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -ex
 
 # This hook is used to ensure we are using a multi-arch capable builder
+# moby/buildkit image version should be consistent w/ tonistiigi/binfmt
+# "docker exec -it buildx_buildkit_cbmultiarch0 /usr/bin/buildkit-qemu-aarch64 --version"
+# Ideally should match "tonistiigi/binfmt:qemu-<version>" installed on the host
+# If they are too far apart, it could lead to undesired build corruption.
+# Before upgrading qemu version, we need to make sure it is compatible with the OS and its glibc
 
 # First be sure dockerd is available and writable by 'couchbase' user
 if ! sudo -u couchbase find /var/run/docker.sock -writable ; then exit 0; fi
@@ -12,6 +17,6 @@ sudo -u couchbase --set-home --preserve-env \
   then
     docker buildx rm cbmultiarch;
   fi;
-  docker buildx create --name cbmultiarch --driver docker-container --use --platform linux/amd64,linux/arm64;
+  docker buildx create --name cbmultiarch --driver docker-container --driver-opt image=moby/buildkit:v0.23.2 --use --platform linux/amd64,linux/arm64;
   docker buildx inspect --bootstrap;
 '
